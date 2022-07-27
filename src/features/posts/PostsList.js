9151d450bb61b48722ad9bb3f40fe8cc90618bd4
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-07-27 17:38:19 -0300

    Using Redux Data

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { PostAuthor } from './PostAuthor';
+import { ReactionButtons } from './ReactionButtons';
+import { TimeAgo } from './TimeAgo';
 
 export const PostsList = () => {
   const posts = useSelector(state => state.posts);
 
-  const renderedPosts = posts.map(post => (
+  // Sort posts in reverse chronological order by datetime string
+  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+
+  const renderedPosts = orderedPosts.map(post => (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
+      <PostAuthor userId={post.userId} />
+      <TimeAgo timestamp={post.date} />
       <p className="post-content">{post.content.substring(0, 100)}</p>
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
+      <ReactionButtons post={post} />
     </article>
   ));
 
